2a23b7bb3085ef1e4ac400cea39d7c16bb95f875
Yeshey <yesheysangpo@hotmail.com>
2021-08-28 02:20:48 +0100

    adding some cookies & cleaning

--- scripts2.js
+++ scripts2.js
@@ -1,25 +1,30 @@
 // Method 2, Less preety, More accurate
-/*
-var count = 0;
 
-  document.body.innerHTML = Math.round(count);
-*/
+//from https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
 
-/*function boom(){
-    //alert("hello");
-    document.querySelector('.estuda').textContent=Date.now();
-}*/
-
-//document.querySelector('.estuda').textContent='Clicks';
-
-//var myVar = setInterval(myTimer, 1000);
-
-
-/*var clicks=0;
-
-var textNode = document.getElementById(badge).firstChild;
-var start = Date.now();
-*/
+//eraseCookie('clicks');
 
 //https://www.w3schools.com/js/js_cookies.asp
 //better: https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
@@ -30,6 +35,18 @@ console.log(clicks);
 
 var clicks=0,CPS=0, CPSPS=0, CPS3=0;
 
+// Cookies testing
+var clickscookie = parseInt(getCookie('clicks'));
+var CPScookie = parseInt(getCookie('CPS'));
+var CPSPScookie = parseInt(getCookie('CPSPS'));
+if (clickscookie) {
+    clicks=clickscookie; }
+if (CPScookie) {
+    CPS=CPScookie; }
+if (CPSPScookie) {
+    CPSPS=CPSPScookie; }
+
+
 let clickspan = document.querySelector(".clicksspan");
 let CPSspan = document.querySelector(".CPSspan");
 let CPSPSspan = document.querySelector(".CPSPSspan");
@@ -39,7 +56,6 @@ clickspan.addEventListener('click', function(){
     //alert("hello");
     clicks++;
 
-
     //clickspan.setAttribute('style','color:white');
     
     //setTimeout(500);
@@ -47,7 +63,7 @@ clickspan.addEventListener('click', function(){
     //clickspan.setAttribute('style','color:black transition: 0.3s; delay:0.5s');
     //licks = Math.round((new Date()-Date.now()));
     //document.querySelector(".clicks2").textContent=(clicks + " Clicks");
-    updateclicks();
+    updatevisable();
 })
 CPSspan.addEventListener('click', function(){
     //alert("hello");
@@ -64,19 +80,6 @@ CPS3span.addEventListener('click', function(){
     CPS3++;
     CPS3span.innerHTML=CPS3.toFixed(1) + ' $/s^3';
 })
-
-/*  //VAriable repeating function
-function periodicall() {
-    setTimeout(periodicall, (1000/CPS));
-    if (CPS!=0){
-        //clicks++;
-    }
-};
-periodicall();
-*/
-
-
-//----------------
 
 //https://stackoverflow.com/questions/18368095/how-to-grow-number-smoothly-from-0-to-5-000-000/18391316
 function timedCounter(finalValue, milliseconds, startValue, callback){
@@ -99,9 +102,6 @@ function timedCounter(finalValue, milliseconds, startValue, callback){
     })();
   
   }
-  
-
-
 
 window.setInterval(function update() {
     //document.querySelector('.estuda').textContent=Date.now();
@@ -110,53 +110,46 @@ window.setInterval(function update() {
     CPSPS+=CPS3/10;
     CPS+=CPSPS/10;
     clicks+=CPS/10;
-    //updateclicks();
+    //updatevisable();
 
     timedCounter((clicks+(CPS/10)), 100, clicks, function(value){
         clickspan.innerHTML=value.toFixed(1) + ' $';
-        // Do something with value
-      });
-
-      if (clicks>=10 && clicks<50){
-        //alert("tada");
-        CPSspan.setAttribute('style', 'display:initial;');
-    }
-    if (clicks>=150 && clicks<400){
-        //alert("tada");
-        CPSPSspan.setAttribute('style', 'display:initial;');
-    }
-    if (clicks>=1000 && clicks<1500){
-        //alert("tada");
-        CPS3span.setAttribute('style', 'display:initial;');
-    }
+        setCookie('clicks',clicks,60);
+    });
 
     timedCounter((CPS+(CPSPS/10)), 100, CPS, function(value){
         CPSspan.innerHTML=value.toFixed(1) + ' $/sec';
-        // Do something with value
-      });
+        setCookie('CPS',CPS,60);
+    });
 
     timedCounter((CPSPS+(CPS3/10)), 100, CPSPS, function(value){
         CPSPSspan.innerHTML=value.toFixed(1) + ' $/s/s';
-        // Do something with value
-      });
+        setCookie('CPSPS',CPSPS,60);
+    });
+
+    updatevisable()
 
 }, 100);
-function updateclicks(){
+
+function updatevisable(){
     clickspan.innerHTML=clicks.toFixed(1) + ' $';
     CPSspan.innerHTML=CPS.toFixed(1) + ' $/sec';
     CPSPSspan.innerHTML=CPSPS.toFixed(1) + ' $/s/s';
     CPS3span.innerHTML=CPS3.toFixed(1) + ' $/s^3';
     //Math.round(number * 10)
-    if (clicks>=10 && clicks<50){
+    if (clicks>=10 && clicks<150){
         //alert("tada");
         CPSspan.setAttribute('style', 'display:initial;');
     }
-    if (clicks>=150 && clicks<400){
+    if (clicks>=150 && clicks<1000){
         //alert("tada");
+        CPSspan.setAttribute('style', 'display:initial;');
         CPSPSspan.setAttribute('style', 'display:initial;');
     }
-    if (clicks>=1000 && clicks<1500){
+    if (clicks>=1000){
         //alert("tada");
+        CPSspan.setAttribute('style', 'display:initial;');
+        CPSPSspan.setAttribute('style', 'display:initial;');
         CPS3span.setAttribute('style', 'display:initial;');
     }
 }
@@ -169,14 +162,6 @@ function show(){
     CPS=0;
     clicks=0;
 
-    updateclicks();
+    updatevisable();
 }
 
-/*
-setInterval(function(){
-
-    clicks++;
-    document.querySelector('.estuda').textContent=clicks;
-    //alert("clicks has " + clicks);
-}, 1);
-*/
