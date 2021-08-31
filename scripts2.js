5210ae186697519e19466870548fba3f39fbf5b9
Yeshey <yesheysangpo@hotmail.com>
2021-09-01 00:22:02 +0100

    making it SUPER obvious that you have to press the button

--- scripts2.js
+++ scripts2.js
@@ -23,8 +23,26 @@ function getCookie(name) {
 function eraseCookie(name) {   
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
-
 //eraseCookie('clicks');
+
+// mouse tracking effect
+let box = document.querySelector(".mouse");
+let boxBoundingRect = box.getBoundingClientRect();
+let boxCenter= {
+	x: boxBoundingRect.left + boxBoundingRect.width/2, 
+  y: boxBoundingRect.top + boxBoundingRect.height/2
+};
+document.addEventListener("mousemove", e => {
+	//let angle = Math.atan2(e.pageX - boxCenter.x, - (e.pageY - boxCenter.y) )*(180 / Math.PI);	    
+    dist_box_mouse=Math.sqrt((e.pageX-boxCenter.x)^2+(e.pageY-boxCenter.y)^2)
+    dist_mouse_x=e.pageX-boxCenter.x
+    dist_mouse_y=e.pageY-boxCenter.y
+    console.log(dist_mouse_y)
+    box.style.transform = `translate(${dist_mouse_x/200}px,${dist_mouse_y/200}px)`;  
+	//box.style.transform = `translate(${e.pageY/100}px,${e.pageX/100}px)`;  
+    //box.style.transform = `translate()`;  
+})
+
 
 var clicks=0,CPS=0, CPSPS=0, CPS3=0;
 
@@ -143,6 +161,18 @@ function updatevisable(){
         CPSspan.setAttribute('style', 'display:inline-block;');
         CPSPSspan.setAttribute('style', 'display:inline-block;');
         CPS3span.setAttribute('style', 'display:inline-block;');
+        // make the hover less intrusive now 
+        var el = document.querySelector('.firsthover_r');
+        if (el.classList.contains("firsthover_r")) {
+            el.classList.remove("firsthover_r");
+            el.classList.add("firsthover_b");
+        }
+        el = document.querySelectorAll('.firsthover_s_r');
+        var i;
+        for (i = 0; i < el.length; i++) {
+            el[i].classList.remove("firsthover_s_r");
+            el[i].classList.add("firsthover_s_b");
+        }
     }
 }
 
